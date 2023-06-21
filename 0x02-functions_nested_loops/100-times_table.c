883b217cfa309ad85110ea437859900124ce8931
khauki <www.hopekhauki@gmail.com>
2023-06-21 13:21:54 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,30 +1,33 @@
-#include "main.h"
+#include “main.h”
+#include<stdio.h>
 /**
- * jack_bauer - Print every minute of the day of Jack Bauer
- * starting from 00:00 to 23:59
+ * print_to_98 - Check main.h
+ * @n: An input value
+ * Description: This function prints all natural numbers from n to 98
+ * Return: Nothing
  */
-void jack_bauer(void)
+void print_to_98(int n)
 {
-	int a, b, c, d;
-
-	for (a = 0; a <= 2; a++)
+	if (n <= 98)
 	{
-	for (b = 0; b <= 9; b++)
-	{
-	if ((a <= 1 && b <= 9) || (a <= 2 && b <= 3))
-	{
-	for (c = 0; c <= 5; c++)
-	{
-	for (d = 0; d <= 9; d++)
-	{
-	_putchar(a + '0');
-	_putchar(b + '0');
-	_putchar(58);
-	_putchar(c + '0');
-	_putchar(d + '0');
-	_putchar('\n');
+		for (; n <= 98; n++)
+		{
+			printf(“%d”, n);
+			if (n == 98)
+				break;
+			printf(“, “);
+		}
+		printf(“\n”);
 	}
-	}
-	}
+	else
+	{
+		for (; n >= 98; n--)
+		{
+			printf(“%d”, n);
+			if (n == 98)
+				break;
+			printf(“, “);
+		}
+		printf(“\n”);
 	}
 }
