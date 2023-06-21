7f00e037f3cf9ffdb5e0b6d6a0b315e97f9b4601
khauki <www.hopekhauki@gmail.com>
2023-06-21 06:26:02 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,39 +1,29 @@
-#include <stdio.h>
 #include "main.h"
 /**
- * print_times_table - Prints the times table of the input,
- *                     starting with 0.
- * @n: The value of the times table to be printed.
+ * jack_bauer - Print every minute of the day of Jack Bauer
+ * starting from 00:00 to 23:59
  */
-void print_times_table(int n)
+void jack_bauer(void)
 {
-	int num, mult, prod;
-	if (n >= 0 && n <= 15)
-	{
-		for (num = 0; num <= n; num++)
-		{
-			_putchar('0');
-			for (mult = 1; mult <= n; mult++)
-			{
-				_putchar(',');
-				_putchar(' ');
-				prod = num * mult;
-				if (prod <= 99)
-					_putchar(' ');
-				if (prod <= 9)
-					_putchar(' ');
-				if (prod >= 100)
-				{
-					_putchar((prod / 100) + '0');
-					_putchar(((prod / 10)) % 10 + '0');
-				}
-				else if (prod <= 99 && prod >= 10)
-				{
-					_putchar((prod / 10) + '0');
-				}
-				_putchar((prod % 10) + '0');
-			}
-			_putchar('\n');
-		}
-	}
+        int a, b, c, d;
+        for (a = 0; a <= 2; a++)
+        {
+        for (b = 0; b <= 9; b++)
+        {
+        if ((a <= 1 && b <= 9) || (a <= 2 && b <= 3))
+        {
+        for (c = 0; c <= 5; c++)
+        {
+        for (d = 0; d <= 9; d++)
+        {
+        _putchar(a + '0');
+        _putchar(b + '0');
+        _putchar(58);
+        _putchar(c + '0');
+        _putchar(d + '0');
+        _putchar('\n');
+        }
+        }
+        }
+        }
 }
