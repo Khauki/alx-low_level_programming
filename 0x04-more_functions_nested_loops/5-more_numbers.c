d7399b11fe605c35c35afc0a2453dfeab28a5622
khauki <www.hopekhauki@gmail.com>
2023-06-23 16:51:51 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,12 +8,14 @@ void more_numbers(void)
 {
 	int i, j;
 
-	for (j = 0; j < 10; j++)
+	for (i = 1; i <= 10; i++)
 	{
-		for (i = 0; i <= 14; i++)
+		for (j = 0; j <= 14; j++)
 		{
-			printf("%02d", i);
+			if (j >= 10)
+				_putchar('1');
+			_putchar(j % 10 + '0');
 		}
+		_putchar('\n');
 	}
-	putchar('\n');
 }
