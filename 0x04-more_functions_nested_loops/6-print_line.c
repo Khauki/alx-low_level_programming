ecfa7392afc3b14e922820dea6ed2540800983fb
khauki <www.hopekhauki@gmail.com>
2023-06-23 10:40:58 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_line -straight-line
+ * @n: no times
+ * Return: 0
+ */
+void print_line(int n)
+{
+	int n;
+
+	for(i = 0; 1 < n; i++)
+	{
+		putchar('_');
+	}
+	putchar ('\n');
+}
