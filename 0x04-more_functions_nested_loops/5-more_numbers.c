bb49c5c79ec1c7a7b852ade6999a2843be7d32ee
khauki <www.hopekhauki@gmail.com>
2023-06-23 16:02:17 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,19 +1,11 @@
-#include "main.h"
+#include <stdio.h>
 
-/**
- * more_numbers - print more numbers
- */
-void more_numbers(void)
-{
+void more_numbers(void) {
 	int i, j;
-
-	for (i = 1; i <= 10; i++)
-	{
-		for (j = 0; j < 14; j++)
-		{
-			if (j >= 10)
-				putchar ('1');
-			putchar (j % 10 + 0);
+	for (j = 0; j < 10; j++) {
+		for (i = 0; i <= 14; i++) {
+			printf("%02d", i);
 		}
 	}
+	putchar('\n');
 }
