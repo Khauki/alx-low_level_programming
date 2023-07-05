3a3f089f88956b2a4d852646d8ddd81374df92df
khauki <www.hopekhauki@gmail.com>
2023-07-05 10:07:16 -0700

    inception

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -16,17 +16,17 @@ int is_prime_number(int n)
 }
 
 /**
- * actaul_prime - calculate if a number is prime recursive
+ * actual_prime - calculate if a number is prime recursive
  * @n: number to evaluate
  * @i: iterator
  *
  * Return: 1 if n is prime, 0 if not
  */
-int actaul_prime(int n, int i)
+int actual_prime(int n, int i)
 {
 	if (i == 1)
 		return (1);
 	if (n % i == 0 && i > 0)
 		return (0);
-	return (actaul_prime(n, i - 1));
+	return (actual_prime(n, i - 1));
 }
