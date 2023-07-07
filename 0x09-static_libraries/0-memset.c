ee761f4fb8abdf2b4077d31e78d1e07af4433964
khauki <www.hopekhauki@gmail.com>
2023-07-07 11:48:44 -0700

    luxury

--- 0x09-static_libraries/0-memset.c
+++ 0x09-static_libraries/0-memset.c
@@ -1,1 +1,19 @@
-char *_memset(char *s, char b, unsigned int n);
+#include "main.h"
+
+/**
+ *_memset - Entry point
+ * @s: pointed destination
+ * @b: constant byte
+ * @n: bytes
+ * Return: 0 (Success)
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+        unsigned int i;
+
+        for (i = 0; i < n; i++)
+        {
+                s[i] = b;
+        }
+        return (s);
+}
--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,1 +1,15 @@
-void _puts(char *s);
+#include "main.h"
+
+/**
+ * _puts - Puts a string
+ * @str: the string to print
+ * Return: void
+ */
+void _puts(char *str)
+{
+        for (; *str != '\0'; str++)
+        {
+                putchar(*str);
+        }
+        putchar('\n');
+}
