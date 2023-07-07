05250f2d7cf9e93d45d237ab2b05ae660e7df0b2
khauki <www.hopekhauki@gmail.com>
2023-07-07 12:56:36 -0700

    luxury

--- 0x09-static_libraries/1-isdigit.c
+++ 0x09-static_libraries/1-isdigit.c
@@ -1,1 +1,17 @@
-int _isdigit(int c);
+include "main.h"
+
+/**
+ * _isdigit - check if numbers are 0 - 9
+ * @c: char to checked
+ *
+ * Return: 1 or 0
+ */
+
+int _isdigit(int c)
+{
+        if (c >= 48 && c <= 57)
+        {
+                return (1);
+        }
+        return (0);
+}
