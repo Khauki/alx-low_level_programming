e5cbab6deb282be0817089b0840823a0f68469d8
khauki <www.hopekhauki@gmail.com>
2023-07-11 07:42:12 -0700

    C-static libraries

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -3,33 +3,19 @@
 /**
  * _ato1 - Convert a string to an interger
  * @s: The pointer to convert
- * Return: A integer
+ * Return: An integer
  */
 int _atoi(char *s)
 {
-        int c = 0;
-        unsigned int ni = 0;
-        int min = 1;
-        int isis = 0;
-
-        while (s[c])
-        {
-                if (s[c] == 45)
-                {
-                        min *= -1;
-                }
-                while (s[c] >= 48 && s[c] <= 57)
-                {
-                        isis = 1;
-                        ni = (ni * 10) + (s[c] - '0');
-                        c++;
-                }
-                if (isis == 1)
-                {
-                        break;
-                }
-                c++;
-                {
-                ni *= (ni);
-        }}
+	int i = 1;
+	unsigned int num = 0;
+	do {
+		if (*s == '-')
+			i *= -1;
+		else if (*s >= '0' && *s <= '9')
+			num = num * 10 + (*s - '0');
+		else if (num > 0)
+			break;
+	} while (*s++);
+	return (num *i);
 }
