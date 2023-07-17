0ea9974d5a6360fa73449b2356d20be157321725
khauki <www.hopekhauki@gmail.com>
2023-07-17 14:46:58 -0700

    C - Structures, typedef

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -7,11 +7,11 @@
  */
 void free_dog(dog_t *d)
 {
-        if (d)
-        {
-                free(d->name);
-                free(d->owner);
-                free(d);
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
 
-        }
+	}
 }
