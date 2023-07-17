1e3bc4643875639f7eed25c2488a8947c34d36f2
khauki <www.hopekhauki@gmail.com>
2023-07-17 11:11:05 -0700

    love yourself

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,15 +14,17 @@ struct dog
         char *name;
         float age;
         char *owner;
-};      
+};
+
 /**
  * god_t - typedef for struct dog
  */
 typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *);
+void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void print_dog(struct dog *d);
+void free_void(dog_t *d);
 char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
 
