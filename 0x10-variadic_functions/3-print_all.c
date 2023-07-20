78fa16742d5e285aeb85933f47bd7968ea473317
khauki <www.hopekhauki@gmail.com>
2023-07-20 11:27:25 -0700

    da file3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -14,6 +14,7 @@ void print_all(const char * const format, ...)
 	va_list list;
 
 	va_start(list, format);
+
 	if (format)
 	{
 		while (format[i])
@@ -21,14 +22,14 @@ void print_all(const char * const format, ...)
 			switch (format[i])
 			{
 				case 'c':
-				printf("%s%c" , sep, va_arg(list, int));
-				break;
+					printf("%s%c" , sep, va_arg(list, int));
+					break;
 				case 'i':
-				printf("%s%d" , sep, va_arg(list, int));
-				break;
+					printf("%s%d" , sep, va_arg(list, int));
+					break;
 				case 'f':
 					printf("%s%f" , sep, va_arg(list, double));
-				break;
+					break;
 				case 's':
 					str = va_arg(list, char *);
 				if (!str)
@@ -38,11 +39,12 @@ void print_all(const char * const format, ...)
 default:
 				i++;
 continue;
+			}
 			sep = ", ";
 			i++;
 			}
 		}
-	}
+
 		printf("\n");
 		va_end(list);
 }
