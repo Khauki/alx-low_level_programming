477508d812983ff1255c6f5fccbdd19f5ffcd0a8
khauki <www.hopekhauki@gmail.com>
2023-11-06 10:54:14 -0800

    processor

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - prints the name of the file
+ *
+ * Return: Always 0 (Sucess)
+ */
 int main(void)
 {
-	printf("%s\n". __TIME__);
-	printf("%s\n". __DATE__);
-	printf("%d\n". __LINE__);
-	printf("%s\n". __FILE__);
+	printf("%s\n", __FILE__);
+	return (0);
 }
