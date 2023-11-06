41075b70f793023b5e6bf4ed1c5ed76c52cb3e1e
khauki <www.hopekhauki@gmail.com>
2023-11-06 11:50:23 -0800

    poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,8 @@
 #ifndef DOG_H
 #define DOG_H
 
-
 /**
- * struct dog - a new type describing a dog
+ * struct-dog - a new tpe descibing a dog
  * @name: the name of the dog
  * @age: the age of the dog
  * @owner: the owner of the dog
@@ -13,8 +12,8 @@ struct dog
 	char *name;
 	float age;
 	char *owner;
-
 };
+
 /**
  * dog_t - typedef for struct dog
  */
